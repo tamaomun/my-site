e6dd71a310e5a1f80e1f11a8adecd85507b79c02
tamasaka <309799481+tamaomun@users.noreply.github.com>
2026-08-14 01:45:27 +0900

    WORKSページを追加

---
+++ src/data/works.ts
@@ -0,0 +1,118 @@
+export const works = [
+  // ──────────────
+  // 漫画
+  // ──────────────
+
+  {
+    type: "comic",
+    tags: ["漫画"],
+    title: "マシシャン達の珍道中",
+    image: "/img/enter1.jpg",
+    buttons: [
+      { text: "第一話", link: "/title01/p1.html" }
+    ]
+  },
+
+  {
+    type: "comic",
+    tags: ["漫画", "ペーパー"],
+    title: "無配ペーパー達",
+    image: "/img/enter2.jpg",
+    description: "イベント参加毎のおまけです",
+    buttons: [
+      { text: "#1", link: "/title02/p1.html" },
+      { text: "#2", link: "/title02/p2.html" },
+      { text: "#3", link: "/title02/p3.html" },
+      { text: "#4", link: "/title02/p4.html" },
+      { text: "#5", link: "/title02/p5.html" },
+      { text: "#6", link: "/title02/p6.html" }
+    ]
+  },
+
+
+  // ──────────────
+  // 一枚絵
+  // ──────────────
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust16.jpg",
+    caption: "合同誌「ピンク」サンプル"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust14.jpg",
+    caption: "合同誌「緑」1"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust15.jpg",
+    caption: "合同誌「緑」2"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust12.jpg",
+    caption: "合同誌「白」1"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust11.jpg",
+    caption: "合同誌「白」2"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust13.jpg",
+    caption: "合同誌「白」3"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "一枚絵"],
+    image: "/img/illust9.jpg",
+    caption: "名刺"
+  },
+
+
+  // ──────────────
+  // 漫画関連イラスト
+  // ──────────────
+
+  {
+    type: "illust",
+    tags: ["イラスト", "漫画関連"],
+    image: "/img/illust8.jpg",
+    caption: "無配表紙"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "漫画関連"],
+    image: "/img/illust10.jpg",
+    caption: "らくがき"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "漫画関連"],
+    image: "/img/illust1.jpg",
+    caption: "サクカ"
+  },
+
+  {
+    type: "illust",
+    tags: ["イラスト", "漫画関連"],
+    image: "/img/illust5.jpg",
+    caption: "太陽初期案"
+  }
+];
