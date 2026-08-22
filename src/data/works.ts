e9bdd5aa107addacf65c47cc6121692f79fbb342
tamasaka <309799481+tamaomun@users.noreply.github.com>
2026-08-22 18:12:51 +0900

    ファビコン変更

--- src/data/works.ts
+++ src/data/works.ts
@@ -16,28 +16,13 @@ export const works = [
     thumbnail: "/img/pinku/illustp1.jpg",
     images: numberedImages("pinku", "illustp", 1),
   },
-{
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃6",
-    thumbnail: "/img/muhai6/muhai6_1.jpg",
-    images: numberedImages("muhai6", "muhai6_", 2),
-    showInAll: false,
-  },
+
   {
     type: "illust",
     tags: ["イラスト"],
     title: "GREEN",
     thumbnail: "/img/midori/illustm1.jpg",
     images: numberedImages("midori", "illustm", 2),
-  },
-  {
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃5",
-    thumbnail: "/img/muhai5/muhai.jpg",
-    images: numberedImages("muhai5", "muhai5_", 3),
-    showInAll: false,
   },
   
   /*
@@ -63,39 +48,7 @@ export const works = [
     title: "白のお姫様",
     thumbnail: "/img/illust9.jpg",
   },
-  {
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃4",
-    thumbnail: "/img/muhai4/illust10.jpg",
-    images: numberedImages("muhai4", "muhai4_", 3),
-    showInAll: false,
-  },
-
-  {
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃3",
-    thumbnail: "/img/muhai3/muhai3_1.jpg",
-    images: numberedImages("muhai3", "muhai3_", 2),
-    showInAll: false,
-  },
-  {
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃2",
-    thumbnail: "/img/muhai2/muhai2_1.jpg",
-    images: numberedImages("muhai2", "muhai2_", 2),
-    showInAll: false,
-  },
-  {
-    type: "comic",
-    tags: ["舞台裏"],
-    title: "無配＃1",
-    thumbnail: "/img/muhai1/muhai1_1.jpg",
-    images: numberedImages("muhai1", "muhai1_", 2),
-    showInAll: false,
-  },
+  
   {
     type: "illust",
     tags: ["イラスト"],
